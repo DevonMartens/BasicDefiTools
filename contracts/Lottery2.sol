456efd08be064e6a2f06f94c0b9e1f09c3753cd9
Devon <dmmartens91@gmail.com>
2022-12-03 21:09:43 -0500

    cleans up lottery contract

--- contracts/Lottery2.sol
+++ contracts/Lottery2.sol
@@ -1,126 +1,141 @@
+// SPDX-License-Identifier: MIT
 /*
-Update the “Lottery” contract from the lecture so that the admin should be able to create multiple bets rather than just one at a time.
-Bonus: Update the code so that one user can only bet once rather than multiple times.
+    WARNING: NOT AUDITED
 */
-// SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.16;
 
-// dont use this with out chainlink users can see this in mem pool
-contract Lottery {
+contract Solution {
 
-    enum State {
-        IDLE, //defulat
-        BETTING
-    }
-    //struct for player 
-    struct LotteryPlayer {
-        address payable playerAddress;
-        uint id;
-        State state;
-    }
-    //mapping id and state
-    mapping(uint => State) public gameState;
-
-    //mapping between struct and address
-    mapping (address => LotteryPlayer) public playerStructs;
-    //players
-    address payable[] public players;
-    //sets a default current state
-    State public currentState = State.IDLE;
-    //storage for max number of players
-    uint public maxPlayers;
-    //same bet amountn 
-    uint public minBetReq;
-    //house takes money
-    uint public houseFee;
-    //storage for admin address
-    address public admin;
-
-    constructor(uint fee){
-        require(fee > 1, "NO_ZERO_FEE");
-        admin = msg.sender;
-        houseFee = fee;
-        
-    }
-    // function to create bet and change enum to active
-    function createRound(uint numPlayers, uint betMoney) external inState(State.IDLE) onlyAdmin {
-        maxPlayers = numPlayers;
-        minBetReq = betMoney;
-        currentState = State.BETTING;
-
-    }
-    //view to check game state 
-
-
-    //place bet
-
-    function placeBet() external payable inState(State.BETTING) {
-        if(msg.value > minBetReq) {
-            revert("NO_CHEAPNESS");
-        }
-        //players is payable so address needs to be
-        players.push(payable(msg.sender));
-
-        if(maxPlayers == players.length) { 
-        uint winner = pickAWinner(maxPlayers);
-        players[winner].transfer((minBetReq * maxPlayers) * (100 - houseFee) / 100);
-        //reset 
-            currentState = State.IDLE;
-        delete players;
-       
-        }
-     
-    }
-    // cancel game
-    function cancelGame()external inState(State.BETTING) onlyAdmin {
-        for(uint i =0; i < players.length; i++){
-            players[i].transfer(minBetReq);
-           
-        }
-         //reset 
-            currentState = State.IDLE;
-            delete players;
-          
-    }
-    //play game
-    function pickAWinner(uint totalPlayers) internal returns(uint) {
-        uint randomNumber;
-        //abi encode packed takes arguments and hashes them
-        //keccak produce hash
-        //cast integer
-        randomNumber = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
-        //ensure its never greater than number of players
-        randomNumber = randomNumber % totalPlayers;
-
-
-        return randomNumber;
-    }
-
-
-        /**  
+    /**  
     ================================================
-    |                  Modifier                    |
+    |               State Variables                |
     ================================================
     **/
-    
-    modifier onlyAdmin() {
-        require(
-            msg.sender == admin, 
-            "YOU_ARE_NOT_ADMIN"
-            );
-            _;
-    }
-    //takes a state 
-    //compares it to current state
-    //this allows for function to require betting mode or idel
 
-    modifier inState(State state) {
-        require(
-            state == currentState,
-            "INVALID_CURRENT_STATE"
-            );
-            _;
+    // Keep track of the current state of the betting round
+    enum State {
+        IDLE,
+        BETTING
     }
-   
+
+    uint public index;
+
+    struct Game {
+        address payable[] players;
+        State currentState;
+        uint maxPlayers;
+        uint minBetReq;
+    }
+    // An array of Game structs
+    Game[] public games;
+
+    // House will take some fee for each round
+    uint public houseFee;
+
+    // Storage for admin address
+    address public admin;
+
+    /**  
+    ================================================
+    |               Constructor.                   |
+    ================================================
+    **/
+
+    constructor(uint fee) {
+        require(fee > 0, "Fee should be greather than 1");
+        admin = msg.sender;
+        houseFee = fee;
+    }
+
+    /**  
+    ================================================
+    |              Game Functions.                 |
+    ================================================
+    **/
+
+    // Create a new game & change the state to State.BETTING
+    function createRound(uint numPlayers, uint betMoney) external onlyAdmin {
+        address payable[] memory _players;
+        games.push(
+            Game({
+                players: _players,
+                currentState: State.BETTING,
+                maxPlayers: numPlayers,
+               minBetReq: betMoney
+            })
+        );
+        index++;
+    }
+
+    // Allow the players to bet =
+    // When we reach the max number of players, we decide the winner
+    function bet(uint gameId) external payable inState(
+        gameId, 
+        State.BETTING) {
+        uint minBetReq = games[gameId].minBetReq;
+        require(
+            msg.value >= minBetReq,
+            "WRONG_AMOUNT"
+        );
+
+        uint maxPlayers = games[gameId].maxPlayers;
+        address payable[] storage players = games[gameId].players;
+        players.push(payable(msg.sender));
+        if (players.length == maxPlayers) {
+            // Pick a winner
+            uint winner = _randomNumber(maxPlayers);
+            // (moneyRequiredToBet * maxPlayers) is the total amount of money 
+            // (100 - houseFee) / 100 takes $ after house
+            address payable winnerPlayer = payable(players[winner]);
+            winnerPlayer.transfer(
+                ((minBetReq * maxPlayers) * (100 - houseFee)) / 100
+            );
+            // Cleanup the data by removing the betting round data
+            delete games[gameId];
+        }
+    }
+
+    // Cancel the current betting round which will change the state to State.IDLE
+    // return funds
+    function cancelGame(uint gameId)
+        external
+        inState(gameId, State.BETTING)
+        onlyAdmin
+    {
+        State currentState = games[gameId].currentState;
+        uint minBetReq = games[gameId].minBetReq;
+        address payable[] storage players = games[gameId].players;
+        for (uint i = 0; i <= players.length - 1; i++) {
+            address payable player = payable(players[i]);
+            player.transfer(minBetReq);
+        }
+        delete games[gameId];
+        currentState = State.IDLE;
+    }
+
+    function _randomNumber(uint _number) internal view returns (uint) {
+     
+        return
+            uint(
+                keccak256(abi.encodePacked(block.timestamp, block.difficulty))
+            ) % _number;
+    }
+
+    /**  
+    ================================================
+    |                  Modifiers                   |
+    ================================================
+    **/
+
+    modifier inState(uint gameId, State state) {
+        State currentState = games[gameId].currentState;
+        require(state == currentState, "WRONG_STATE");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "ONLY_ADMIN");
+        _;
+    }
 }
